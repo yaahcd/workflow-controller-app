9a25f98d4610f3599ba6497b269d3b535bf22d4e
Yara <cremaschi.yara@gmail.com>
2023-11-05 10:13:15 +0000

    added some styling changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import {
-  LogoContainer,
   NavLinks,
   NavbarContainer,
   NavLink,
@@ -10,8 +9,8 @@ function Navbar() {
   return (
     <>
       <NavbarContainer>
-        <LogoContainer></LogoContainer>
         <NavLinks>
+          <NavLink to="/home">Home</NavLink>
           <NavLink to="employees">Employees</NavLink>
           <NavLink to="jobs">Jobs</NavLink>
           <NavLink to="">Map</NavLink>
--- src/components/navbar/navbar.styles.jsx
+++ src/components/navbar/navbar.styles.jsx
@@ -2,12 +2,12 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const NavbarContainer = styled.div`
-  height: 60px;
+  height: 70px;
   width: 100%;
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
-  background-color: #ADB5BD;
+  background-color: #457B9D;
 `;
 
 export const LogoContainer = styled(Link)`
@@ -17,11 +17,11 @@ export const LogoContainer = styled(Link)`
 `;
 
 export const NavLinks = styled.div`
-  width: 50%;
+  width: 40%;
   height: 100%;
   display: flex;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-evenly;
 `;
 
 export const NavLink = styled(Link)`
@@ -29,10 +29,17 @@ export const NavLink = styled(Link)`
   font-weight: bolder;
   font-size: 18px;
   color: black;
-  padding: 10px 15px;
+  padding: 15px 20px;
   cursor: pointer;
+  background-color: #1D3557;
+  color: white;
+  border-radius: 10px;
+  box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
+  text-shadow: 1px 0px 1px rgba(128, 0, 0, 1);
+  opacity: 0.85;
+  transition: .4s;
 
   &:hover {
-    background-color: #CED4DA;
+    opacity: 1
   }
 `;
